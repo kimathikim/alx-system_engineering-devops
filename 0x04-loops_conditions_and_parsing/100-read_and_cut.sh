b37ad29c0198ef42ee9ce038edd83e1148bd807c
kimathikim <briankimathi94@gmail.com>
2023-05-01 08:06:31 +0300

    loop and parsing

--- 0x04-loops_conditions_and_parsing/100-read_and_cut.sh
+++ 0x04-loops_conditions_and_parsing/100-read_and_cut.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #  Bash script that displays the content of the file /etc/passwd
 
-while EFI= read -r -l; do
-	echo "$l" | cut -d ":" -f1,4,7
+while EFI= read -r l; do
+	echo "$l" | cut -d ":" -f1,3,7
 done <"/etc/passwd"
