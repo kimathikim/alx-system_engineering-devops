752442fb63b446f747dd467ac35cf30cfd2a356d
kimathikim <briankimathi94@gmail.com>
2023-06-25 03:25:37 +0300

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,15 @@
 # use the private key ~/.ssh/school
 # refuse to authenticate using a PasswordAuthenticationd
 
-file { '/home/bk/.ssh/config':
-  ensure  => file,
-  path    => '/home/bk/.ssh/config',
-  content => "Host *\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no\n",
-  mode    => '0600',
-  owner   => 'bk',
+
+file_line { 'Turn off passwd auth':
+  path  => '/home/bk/.ssh/config',
+  line  => '    PasswordAuthentication no',
+  match => '^\s*PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path  => '/home/bk/.ssh/config',
+  line  => '    IdentityFile ~/.ssh/school',
+  match => '^\s*IdentityFile\s+~/.ssh/school',
 }
