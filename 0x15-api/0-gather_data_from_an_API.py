46c28716d8808dabdba9cef255707547468187d5
kimathikim <briankimathi94@gmail.com>
2023-07-30 18:42:37 +0300

    rest Api get method json

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,43 +1,35 @@
 #!/usr/bin/python3
-
 """
-a script that uses restul API to return the
-info of an employee about his TODO list progress
+    script that, using this REST API, for a given employee ID,
+    returns information about his/her TODO list progress.
 """
-import requests
-from sys import argv
-
-
-def todolistGet(id):
-    """function that defines the endpoints and retreived data"""
-
-    # url
-    url = "https://jsonplaceholder.typicode.com"
-
-    # endpoints
-    user = "{}/users/{}".format(url, id)
-    todo = "{}/todos".format(user)
-    task_Done = "{}?completed=true".format(todo)
-
-    # get the data. making the request using get method and
-    # specified format to be used
-
-    user = requests.get(user)
-    user = user.json()
-
-    todo = requests.get(todo)
-    todo = todo.json()
-
-    task_Done = requests.get(task_Done)
-    task_Done = task_Done.json()
-
-    output = "Employee {}: {}".format(user.get("name"), "OK" if len(
-        task_Done) == len(todo) else "Incorrect")
-
-    return output
-
 
 if __name__ == "__main__":
-    employee_id = int(argv[1])
-    result = todolistGet(employee_id)
-    print(result)
+    import requests
+    from sys import argv
+
+    # Data
+    ID = argv[1]
+    url_api = "https://jsonplaceholder.typicode.com"
+
+    # Endpoints
+    ep_user = "{}/users/{}".format(url_api, str(ID))
+    ep_todos = "{}/todos".format(ep_user)
+    ep_todos_completed = "{}?completed=true".format(ep_todos)
+
+    # Requests
+    user = requests.get(ep_user)
+    todos = requests.get(ep_todos)
+    todos_completed = requests.get(ep_todos_completed)
+
+    # Format
+    user = user.json()
+    todos = todos.json()
+    completed = todos_completed.json()
+
+    # Output
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)
+    ))
+    for task in completed:
+        print("\t {}".format(task.get("title")))
