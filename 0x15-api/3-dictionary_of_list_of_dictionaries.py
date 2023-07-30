3bbc7e038fdb2e916f18df86d2aa60487e1d0a83
kimathikim <briankimathi94@gmail.com>
2023-07-30 22:46:44 +0300

    rest Api export the entire data to  json

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import json
+import requests
+
+
+def todolistGet():
+    """function that defines the endpoints and retrieves data"""
+
+    # url
+    url = "https://jsonplaceholder.typicode.com"
+
+    # endpoint to get users
+    users_endpoint = "{}/users".format(url)
+
+    # get the data. making the request using get method and specified
+    # format to be used
+    users_response = requests.get(users_endpoint)
+    users_data = users_response.json()
+
+    # exporting to json
+    to_json = {}
+    with open("todo_all_employees.json", "w", encoding="utf8") as file:
+        for user in users_data:
+            to_json[user.get("id")] = []
+            todo_endpoint = "{}/todos?userId={}".format(url, user.get("id"))
+            todo_response = requests.get(todo_endpoint)
+            todo_data = todo_response.json()
+            for task in todo_data:
+                to_json[user.get("id")].append({
+                    "task": task.get("title"),
+                    "completed": task.get("completed"),
+                    "username": user.get("username")
+                })
+        json.dump(to_json, file)
+
+
+if __name__ == "__main__":
+    todolistGet()
