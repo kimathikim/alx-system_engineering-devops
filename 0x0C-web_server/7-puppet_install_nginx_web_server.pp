7d10889e89d5ded74995085f86f186cc99af7d41
Brian Kimathi M <briankimathi94@gmail.com>
2023-12-04 21:14:38 +0300

    num of commits

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,46 +1,10 @@
-include ufw
-# Install Nginx web server (w/ Puppet)
-package { 'nginx':
-  package_ensure => installed,
-  service_ensure => 'running',
-}
-
-file{ 'Redirect rule':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=hdZUCjAQaGw permanent;',
-}
-
-file { '/var/www/html/index.html':
-  content => 'Hello World',
-}
-
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
-}
-package {'ufw':
-ensure => installed,
-  }
-
-service {'ufw':
-ensure  => running,
-enable  => true,
-require => Package['ufw'],
-}
-# allow Nginx and SSh
-ufw{'Nginx FULL':
-rule  => 'allow',
-port  => [80,443],
-proto => 'tcp',
-order => 1,
-}
-
-ufw {'SSH':
-rule  => 'allow',
-port  => 22,
-proto => 'tcp',
-order => 2,
+exec { 'configuration':
+  command  => "sudo apt-get -y update && \
+               sudo apt-get -y install nginx && \
+               sudo chown -R ubuntu /var/www && \
+               echo 'Hello World' | sudo tee /var/www/html/index.nginx-debian.html && \
+               sudo sed -i 's/server_name _;/server_name _;\\n\\trewrite ^\\/redirect_me https:\\/\\/github.com\\/MiguelBarreraDev permanent;/' /etc/nginx/sites-available/default && \
+               sudo service nginx start",
+  provider => shell,
 }
 
