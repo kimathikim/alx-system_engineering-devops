82ada8c87a380445251e27ed128c8389b9f1f16b
Brian Kimathi M <briankimathi94@gmail.com>
2023-12-22 10:00:12 +0300

    Back up

--- 0x14-mysql/5-mysql_backup.sql
+++
@@ -1,9 +0,0 @@
-#configurint theslave mysqlserverTOreplicate
-FROM
-the master5.7.22master5.7CHANGE MASTERTOMASTER_HOST = '18.204.10.133',
-MASTER_USER = 'replica_user',
-MASTER_PASSWORD = 'Kimathi@2022',
-#INSERTsample dataTOnexusTABLEINSERT INTOnexusVALUES('1','Kimathi','Mwangi',');
-insert into nexus values('2','Kamau','Mwangi',');INSERT INTOnexusVALUES('3','Kamau','Mwangi',');
-insert into nexus values('4','Kamau','Mwangi',');insert into nexus values('3','Kamau','Mwangi',');
-insert into nexus values('4','Kamau','Mwangi',');
