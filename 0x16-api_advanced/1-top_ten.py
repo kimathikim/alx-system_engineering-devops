2b700a281cbc1f5ecf30f4b8db1b63cda8632b7b
Brian Kimathi M <briankimathi94@gmail.com>
2024-01-26 08:36:32 +0300

    count subscribers

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Function that queries the Reddit API and print the titles of the first 10
+hot posts listed for a given subreddit."""
+
+
+def top_ten(subreddit):
+    """Function that queries the Reddit API
+    and print the titles of the first 10
+    hot posts listed for a given subreddit."""
+    import requests
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+               AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77\
+               Safari/537.36"
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        for i in range(10):
+            print(
+                response.json().get("data").get("children")[i].get("data").get("title")
+            )
+    else:
+        print("None")
