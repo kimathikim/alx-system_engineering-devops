cc963f694c05846d38e7266930d665b05668c742
Brian Kimathi M <briankimathi94@gmail.com>
2024-02-19 09:31:36 +0300

    web monitoring

--- 0x18-webstack_monitoring/api_App.py
+++ 0x18-webstack_monitoring/api_App.py
@@ -1,16 +1,14 @@
-
 """
-Get all hosts for your organization returns "OK" response
+Validate API key returns "OK" response
 """
 
 from datadog_api_client import ApiClient, Configuration
-from datadog_api_client.v1.api.hosts_api import HostsApi
+from datadog_api_client.v1.api.authentication_api import AuthenticationApi
 
 configuration = Configuration()
 with ApiClient(configuration) as api_client:
-    api_instance = HostsApi(api_client)
-    response = api_instance.list_hosts(
-        filter="env:ci",
-    )
+    api_instance = AuthenticationApi(api_client)
+    response = api_instance.validate()
 
     print(response)
+
