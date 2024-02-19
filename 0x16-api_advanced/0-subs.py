5ef3d6e73892a208618568c93e3683fb432f89e0
Brian Kimathi M <briankimathi94@gmail.com>
2024-02-19 10:51:09 +0300

    Subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-"""Function that queries the Reddit API and returns the number of subscribers
-"""
+""" Function that queries the Reddit API """
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """Function that queries the Reddit API and
-    returns the number of subscribers"""
-    import requests
+    """Returns the number of subscribers (not active users, total subscribers)
+    for a given subreddit. If an invalid subreddit is given, the function
+    should return 0.
 
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
-               AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77\
-               Safari/537.36"
-    }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.json().get("data").get("subscribers")
-    else:
+    Args:
+        subreddit: Account to search
+    """
+    if subreddit is None or type(subreddit) is not str:
         return 0
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    User_Agent = 'AgentMEGO'
+    header = {'User-Agent': User_Agent}
+    with requests.Session() as res:
+        data = res.get(url, headers=header)
+        if data.status_code != 200:
+            return 0
+    return data.json().get('data', {}).get('subscribers', 0)
