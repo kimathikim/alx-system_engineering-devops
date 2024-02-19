7a47d404a41f1e31f540993302b7dd75d9ad345f
Brian Kimathi M <briankimathi94@gmail.com>
2024-02-19 10:56:05 +0300

    Recursion

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,33 +2,37 @@
 """A recursive function that queries the reddit API and returns
 a list containing the titles of all hot articles for a given subreddit"""
 import requests
-import sys
+
+def f_after(subreddit, hot_list=[], after=""):
+    """Recursive function find to length of hot_list
+    Args:
+        subreddit: Account to search
+        hot_list: hot list of Reddit
+        after: next page of API
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=100'.format(subreddit)
+    if after is not None:
+        with requests.Session() as res:
+            data = res.get(url,
+                           headers={'User-Agent': 'AgentMEGO'},
+                           params={'after': after})
+            if data.status_code != 200:
+                return None
+            after = data.json().get('data').get('after')
+        hot_list += data.json().get('data').get('children')
+        f_after(subreddit, hot_list, after)
+    return (hot_list)
 
 
-def recurse(subreddit, hot_list=None):
-    if hot_list is None:
-        hot_list = []
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
-               AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77\
-               Safari/537.36"
-    }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json().get("data")
-        titles = data.get("children")
-        print(titles)
-        for title in titles:
-            title = title.get("data").get("title")
-            hot_list.append(title)
-        after = data.get("after")
-        if after:
-            recurse(subreddit, hot_list)
-    else:
-        print("None")
-    return hot_list
+def recurse(subreddit, hot_list=[]):
+    """Returns a list containing the titles of all hot articles for
+    a given subreddit. If no results are found for the given subreddit,
+    the function should return None.
 
+    Args:
+        subreddit: Account to search
+    """
+    if subreddit is None or type(subreddit) is not str:
+        return None
 
-results = recurse(sys.argv[1])
-print(len(results))
+    return f_after(subreddit, hot_list, "")
